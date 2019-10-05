3bc57c8e4db0d897c402271adbcf03a828769807
James Mitchum Lee <jml184@cs18271kq.canterbury.ac.nz>
2019-10-05 17:53:02 +1300

    added navswitch functionality, to toggle bitmaps

--- game.c
+++ game.c
@@ -36,10 +36,11 @@
 #include "pio.h"
 #include "pacer.h"
 
+#include "navswitch.h"
+
 
 /** Define PIO pins driving LED matrix rows.  */
-static const pio_t rows[] =
-{
+static const pio_t rows[] = {
     LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
     LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
     LEDMAT_ROW7_PIO
@@ -47,27 +48,24 @@ static const pio_t rows[] =
 
 
 /** Define PIO pins driving LED matrix columns.  */
-static const pio_t cols[] =
-{
+static const pio_t cols[] = {
     LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
     LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
 };
 
 
-static const uint8_t rock_bitmap[] =
-{
+static const uint8_t rock_bitmap[] = {
     0x00, 0x07, 0x07, 0x07, 0x00
 };
 
 
-static const uint8_t paper_bitmap[] =
-{
+static const uint8_t paper_bitmap[] = {
     0x07, 0x07, 0x07, 0x07, 0x07
+
 };
 
 
-static const uint8_t scissors_bitmap[] =
-{
+static const uint8_t scissors_bitmap[] = {
     0x05, 0x05, 0x02, 0x07, 0x07
 };
 
@@ -96,7 +94,8 @@ static void display_column (uint8_t row_pattern, uint8_t current_column)
 }
 
 
-void led_matrix_init() {
+void led_matrix_init()
+{
 
     /*Initialise LED matrix pins.  */
     pio_config_set (LEDMAT_COL1_PIO, PIO_OUTPUT_HIGH);
@@ -118,24 +117,49 @@ void led_matrix_init() {
 
 int main (void)
 {
+
+
+    uint8_t current_bitmap = 1;
+
+
     uint8_t current_column = 0;
 
+    navswitch_init ();
     system_init ();
     pacer_init (500);
-
     led_matrix_init();
 
 
-    while (1)
-    {
+    while (1) {
         pacer_wait ();
 
-        display_column (scissors_bitmap[current_column], current_column);
+        navswitch_update ();
+
+        if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
+            current_bitmap++;
+            if (current_bitmap > 3) {
+                current_bitmap = 1;
+            }
+        }
+
+        if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
+            current_bitmap--;
+            if (current_bitmap < 1) {
+                current_bitmap = 3;
+            }
+        }
+        if (current_bitmap == 1) {
+            display_column (rock_bitmap[current_column], current_column);
+        } else if (current_bitmap == 2) {
+            display_column (paper_bitmap[current_column], current_column);
+        } else if (current_bitmap == 3) {
+            display_column (scissors_bitmap[current_column], current_column);
+        }
+
 
         current_column++;
 
-        if (current_column > (LEDMAT_COLS_NUM - 1))
-        {
+        if (current_column > (LEDMAT_COLS_NUM - 1)) {
             current_column = 0;
         }
     }
