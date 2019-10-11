bfdbdf2941540797ce51cd8bb76a16313c4d2668
James Mitchum Lee <jml184@cs18257kq.canterbury.ac.nz>
2019-10-11 16:45:48 +1300

    modularization efforts in progress

--- bitmap.h
+++ bitmap.h
@@ -43,8 +43,6 @@ void display_column (uint8_t row_pattern, uint8_t current_column);
 /** displays bitmap by calling display_column  */
 Bitmap_Info display_bitmap(Bitmap_Info bitmap);
 
-/** declares bitmap variables  */
-void bitmaps_init(void);
 
 #endif
 
