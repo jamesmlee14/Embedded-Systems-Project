d9e52c7488450e53b6d414ae76727edc615d4c78
James Mitchum Lee <jml184@cs18257kq.canterbury.ac.nz>
2019-10-11 15:16:01 +1300

    modularized player assignment

--- game.c
+++ game.c
@@ -49,41 +49,7 @@
 #include "ir_uart.h"
 
 #include "bitmap.h"
-
-
-const uint8_t ARROW_BITMAP[] = {
-    0x04, 0x0E, 0x15, 0x04, 0x04
-};
-
-const uint8_t P1_BITMAP[] = {
-    0x02, 0x06, 0x02, 0x02, 0x07
-};
-
-const uint8_t DRAW_BITMAP[] = {
-    0x38, 0x24, 0x24, 0x24, 0x38
-};
-
-/*
-0000 0010
-0000 0110
-0000 0010
-0000 0010
-0000 0111
-*/
-
-const uint8_t P2_BITMAP[] = {
-    0x07, 0x09, 0x02, 0x04, 0x0f
-};
-
-/*
-0000 0111
-0000 1001
-0000 0010
-0000 0100
-0000 1111
-*/
-
-
+#include "player_assignment.h"
 
 
 void led_matrix_refresh(void)
@@ -112,7 +78,6 @@ void led_matrix_refresh(void)
 int main (void)
 {
 
-    Bitmap_Info bitmap = {0, 1, 0, 0};
 
     navswitch_init ();
     system_init ();
@@ -122,8 +87,8 @@ int main (void)
     ir_uart_init();
     button_init();
 
-
-    uint8_t in_player_phase = 1;
+    Bitmap_Info bitmap = {0, 4, 0, 0};
+    uint8_t in_player_assignment = 1;
     uint8_t in_selection_phase = 0;
     uint8_t in_transmission_phase = 0;
     uint8_t in_outcome_phase = 0;
@@ -137,91 +102,32 @@ int main (void)
 
     led_set (LED1, 0); // LIGHT OFF
 
-    uint8_t current_column = 0;
-
-
 
     while (1) {
 
         pacer_wait ();
         navswitch_update ();
 
-        if (in_player_phase) {
-
-            display_column (ARROW_BITMAP[current_column], current_column);
-
-            current_column++;
-
-            if (current_column > (LEDMAT_COLS_NUM - 1)) {
-                current_column = 0;
-            }
-
-            led_set (LED1, 1); // LIGHT ON
-
-            button_update ();
-
-            if (button_push_event_p (BUTTON1)) {
-                is_player1 = 'T';
-                ir_uart_putc('T');  //sending
-            }
-            if (ir_uart_read_ready_p()) {
-                char set_player2 = ir_uart_getc();
-                if (set_player2 == 'T') {
-                    is_player2 = set_player2;
-                }
-            }
-
-        }
-
-        if (in_player_phase && (is_player1 == 'T' || is_player2 == 'T')) {
-
-            uint16_t counter = 0;
-            while (counter < 2500) { //wait 5sec
-
-                if (is_player1 == 'T') {
-                    display_column (P1_BITMAP[current_column], current_column);
-
-                    current_column++;
-
-                    if (current_column > (LEDMAT_COLS_NUM - 1)) {
-                        current_column = 0;
-                    }
-
-                } else {
-
-                    display_column (P2_BITMAP[current_column], current_column);
-
-                    current_column++;
-
-                    if (current_column > (LEDMAT_COLS_NUM - 1)) {
-                        current_column = 0;
-                    }
-                }
 
 
-                pacer_wait ();
-                navswitch_update ();
-
-                counter++;
-            }
-
-            counter = 0;
-            while (counter < 500) { //wait 1sec
-                counter++;
-            }
-
-            in_player_phase = 0;
+        //PLAYER ASSIGNMENT
+        if (in_player_assignment) {
+            player_assignment (bitmap, is_player1, is_player2);
             led_matrix_refresh();
-            in_selection_phase = 1;
+
+            if (is_player1 == 'T' || is_player2 == 'T') {
+                led_matrix_refresh();
+                bitmap.current_bitmap = 1;
+                in_player_assignment = 0;
+                in_selection_phase = 1;
+            }
         }
-
-
 
 
         if (in_selection_phase && !bitmap.locked_in) {
             led_matrix_refresh();
             led_set (LED1, 0); // LIGHT OFF
-            bitmap = display_bitmap(bitmap.current_column, bitmap.current_bitmap, bitmap.opponent_bitmap, bitmap.locked_in);
+            bitmap = display_bitmap(bitmap);
             if (bitmap.locked_in) {
                 in_transmission_phase = 1;
                 led_matrix_refresh();
@@ -264,40 +170,80 @@ int main (void)
             while (counter < 2500) { //wait 5sec
                 pacer_wait ();
                 counter++;
-                bitmap = display_bitmap(bitmap.current_column, bitmap.current_bitmap, bitmap.opponent_bitmap, bitmap.locked_in);
+                bitmap = display_bitmap(bitmap);
             }
 
             in_outcome_phase = 0;
 
-            char outcome = 'D';
+            char outcome = 'X';
 
-/*
+            // win
+
+            // loss
+
             if (bitmap.current_bitmap == bitmap.opponent_bitmap) { // draw
                 outcome = 'D';
             }
-*/
+
             counter = 0;
             while (counter < 2500) { //wait 5sec
-                led_set (LED1, 1); // LIGHT ON
                 pacer_wait ();
                 counter++;
 
                 led_matrix_refresh();
-                display_column (DRAW_BITMAP[current_column], current_column);
 
-                current_column++;
+                if (outcome == 'W') {
+                    led_set (LED1, 1); // LIGHT ON
+                    bitmap.current_bitmap = 7;
 
-                if (current_column > (LEDMAT_COLS_NUM - 1)) {
-                    current_column = 0;
+                } else if (outcome == 'L') {
+                    bitmap.current_bitmap = 8;
+
+                } else if (outcome == 'D') {
+                    bitmap.current_bitmap = 9;
                 }
+                bitmap = display_bitmap(bitmap);
+
+            }
 
 
+            counter = 0;
+            while (counter < 2500) { //wait 5sec
+                pacer_wait ();
+                counter++;
+
+                led_matrix_refresh();
+
+                if (player_score == 0) {
+                    bitmap.current_bitmap = 13;
+                } else if (player_score == 1) {
+                    bitmap.current_bitmap = 11;
+                } else if (player_score == 2) {
+                    bitmap.current_bitmap = 12;
+                } else if (player_score == 3) {
+                    bitmap.current_bitmap = 13;
+                }
+                bitmap = display_bitmap(bitmap);
 
             }
 
 
 
-        led_matrix_refresh();
+            led_matrix_refresh();
+            led_set (LED1, 0); // LIGHT OFF
+
+
+            bitmap.current_column = 0;
+            bitmap.current_bitmap = 1;
+            bitmap.opponent_bitmap = 0;
+            bitmap.locked_in = 0;
+
+            in_selection_phase = 1;
+            in_transmission_phase = 0;
+            in_outcome_phase = 0;
+            sent = 0;
+            recieved = 0;
+
         }
 
 
