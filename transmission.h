8d5293028c9bcbcdaf189ae713c24f3bb7a5538a
James Mitchum Lee <jml184@cs18281kq.canterbury.ac.nz>
2019-10-12 15:35:00 +1300

    modularized transmission phase

---
+++ transmission.h
@@ -0,0 +1,37 @@
+/*
+ * transmission.h
+ *
+ * Copyright 2019 James Mitchum Lee <jml184@cs18254kq>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+
+#ifndef TRANSMISSION_H
+#define TRANSMISSION_H
+
+#include "system.h"
+#include "bitmap.h"
+#include "ir_uart.h"
+
+
+/** Transmission Phase  */
+Bitmap_Info transmission(Bitmap_Info bitmap);
+
+
+#endif
