422f9c2d61121f57acd35244e1b3a3fac1edeaef
James Mitchum Lee <jml184@cs18281kq.canterbury.ac.nz>
2019-10-12 18:19:31 +1300

    modularized the outcome phase

--- game.c
+++ game.c
@@ -42,18 +42,7 @@
 #include "player_assignment.h"
 #include "selection.h"
 #include "transmission.h"
-
-
-
-int get_result(int current, int opponent)
-{
-    current++;
-    opponent++;
-    int result = (current - opponent + 3) % 3;
-    return result;
-}
-
-
+#include "outcome.h"
 
 int main (void)
 {
@@ -65,14 +54,12 @@ int main (void)
     ir_uart_init();
     button_init();
 
-    Player_Bitmap bitmap = {0, 4, 0, 0, 0};
+    Player_Bitmap bitmap = {0, 4, 0, 0, 0, 0};
     uint8_t in_player_assignment = 1;
     uint8_t in_selection_phase = 0;
     uint8_t in_transmission_phase = 0;
     uint8_t in_outcome_phase = 0;
 
-
-    uint8_t player_score = 0;
 
     led_set (LED1, 0); // LIGHT OFF
 
@@ -124,82 +111,19 @@ int main (void)
         }
 
         // OUTCOME
-
-
         if (in_outcome_phase) {
 
-            uint16_t counter = 0;
-            while (counter < 2500) {    //display both bitmaps for 5 sec
-                pacer_wait ();
-                counter++;
-                bitmap = display_bitmap(bitmap);
-            }
-
-
-            int outcome = get_result(bitmap.current_bitmap, bitmap.opponent_bitmap);
-
-            if (outcome == 1) { // WIN
-                    player_score++;
-                    led_set (LED1, 1); // LIGHT ON
-                    bitmap.current_bitmap = 7;
-
-                } else if (outcome == 2) {   // LOSS
-                    bitmap.current_bitmap = 8;
-
-                } else if (outcome == 0) {   // DRAW
-                    bitmap.current_bitmap = 9;
-                }
-
-            counter = 0;
-            bitmap_refresh();
-
-            while (counter < 2500) { //display outcome for 5sec
-
-                pacer_wait ();
-                counter++;
-                bitmap = display_bitmap(bitmap);
-
-            }
-
-            if (player_score == 0) {
-                    bitmap.current_bitmap = 10;
-                } else if (player_score == 1) {
-                    bitmap.current_bitmap = 11;
-                } else if (player_score == 2) {
-                    bitmap.current_bitmap = 12;
-                } else if (player_score == 3) {
-                    bitmap.current_bitmap = 13;
-                }
-
-            counter = 0;
-            bitmap_refresh();
-            led_set (LED1, 0); // LIGHT OFF
-            while (counter < 2500) { //display score for 5sec
-
-                pacer_wait ();
-                counter++;
-                bitmap = display_bitmap(bitmap);
-
-            }
-
-
-
-            bitmap_refresh();
-            led_set (LED1, 0); // LIGHT OFF
-
-
-            bitmap.current_column = 0;
-            bitmap.current_bitmap = 1;
-            bitmap.opponent_bitmap = 0;
-            bitmap.locked_in = 0;
+            bitmap = outcome(bitmap);
 
             in_selection_phase = 1;
             in_transmission_phase = 0;
             in_outcome_phase = 0;
-
-
             in_outcome_phase = 0;
+
         }
+
+
+
 
 
 
