20d6fa2a3e4706a2a90587515b40a1ef3941e1bf
James Mitchum Lee <jml184@cs18281kq.canterbury.ac.nz>
2019-10-12 15:34:56 +1300

    modularized transmission phase

---
+++ transmission.c
@@ -0,0 +1,63 @@
+/*
+ * transmission.c
+ *
+ * Copyright 2019 James Mitchum Lee <jml184@cs18254kq>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+#include "system.h"
+#include "bitmap.h"
+#include "ir_uart.h"
+
+Bitmap_Info transmission(Bitmap_Info bitmap)
+{
+    uint8_t sent = 0;
+    char recieved = 0;
+
+    led_set (LED1, 1); // LIGHT ON
+    ir_uart_init();
+    while (recieved == 0) {
+
+        if (!sent) {
+
+            ir_uart_putc(bitmap.current_bitmap);  //sending
+            sent = 1;
+
+        }
+
+        if (ir_uart_read_ready_p() && recieved == 0) {
+            recieved = ir_uart_getc();
+        }
+
+
+        if (sent) {
+            if (recieved == 1 || recieved == 2 || recieved == 3) {
+                bitmap.opponent_bitmap = recieved; //recieving
+            } else {
+                recieved = 0;
+            }
+        }
+
+    }
+    led_set (LED1, 0); // LIGHT OFF
+    return bitmap;
+
+
+
+}
